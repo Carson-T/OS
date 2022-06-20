734bd8ac262ee3b3c8bd2ee18486b27b0dd828ee
Carson-T <tan_biubiu@qq.com>
2022-06-20 16:02:00 +0800

    final

---
+++ test1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+int main(){
+    fork();
+    fork();
+    fork();
+    printf("a");
+    
+
+    // fork();
+    // fork();
+    // fork();
+    // printf("a\n");
+
+
+    // for(int i=0;i<3;i++){
+    //     fork();
+    //     fork();
+    //     printf("A");
+    // }
+    // fork();
+    // printf("B");
+}
